39e66e09c1e3adca399397a0a7ed7289ec1bd479
Jana <Jana@Janka-PC>
2015-05-12 01:02:05 +0200

    Merge origin/master

---
+++ src/main/java/cz/muni/fi/pb138/silverspooncamelroutes/XSLTransformatorException.java
@@ -0,0 +1,18 @@
+package cz.muni.fi.pb138.silverspooncamelroutes;
+
+public class XSLTransformatorException extends RuntimeException {
+
+    public XSLTransformatorException(String msg) {
+        super(msg);
+    }
+
+    public XSLTransformatorException(Throwable cause) {
+        super(cause);
+    }
+
+    public XSLTransformatorException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
+
